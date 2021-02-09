f31a6d8c358ae40fb174274f088bd47c52498c8a
django <jannuraidynuly@gmail.com>
2021-02-09 15:36:02 +0600

    add file conftest.py

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language',
+                     action='store',
+                     default='en',
+                     help="Choose language: ru or en")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nstart browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquite browser..")
+    browser.quit()
