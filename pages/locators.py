ffa38a127285097b5d266502afd9710d6fa6e694
Zhannur <jannuraidynuly@gmail.com>
2021-02-13 10:57:06 +0600

    Implemented transition between pages

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,5 @@ class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators:
-    LOGIN_FORM = (By.CSS_SELECTOR, "#alogin_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#aregister_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,7 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.is_element_present(*MainPageLocators.LOGIN_LINK)
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
