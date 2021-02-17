4e1015bd3f028ba5e85c24db64a16f339b07f8ed
django <jannuraidynuly@gmail.com>
2021-02-17 12:05:26 +0600

    delete

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,7 +10,6 @@ class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
         login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
@@ -20,9 +19,9 @@ class BasePage:
         basket_page = self.browser.find_element(*BasePageLocators.BASKET_LINK)
         basket_page.click()
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what, timeout=4):
         try:
-            self.browser.find_element(how, what)
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except NoSuchElementException:
             return False
         return True
